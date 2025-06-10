a1593ea344589f665baab835739f297cffa0f798
Evgeniya <evgeniya.a.golubeva.2021@elsys-bg.org>
2025-06-10 11:15:52 +0300

    Docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './ThreeDify-frontend/src/setupTests.js', 
+  root: 'ThreeDify-frontend',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
   },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'ThreeDify-frontend/src')
+    }
+  }
 })
